e5c6eced84d316bc1a3167748ca4d1514cf8e8b8
Rafael <rapha.pse@outlook.com>
2017-09-04 21:25:24 -0300

    Criando QUADRADO em C

---
+++ exec-c/quadrado/main.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Rafael
+ *
+ * Created on 4 de Setembro de 2017, 20:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("########\n");
+    printf("#      #\n");
+    printf("#      #\n");
+    printf("#      #\n");
+    printf("#      #\n");
+    printf("########\n");
+    return (EXIT_SUCCESS);
+}
+
