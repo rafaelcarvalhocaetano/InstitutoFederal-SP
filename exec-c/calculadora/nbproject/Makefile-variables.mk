af2abb1e30adecfc63acfba6faa14b81b5b888c6
Rafael <rapha.pse@outlook.com>
2017-09-04 21:24:04 -0300

    Calculadora em C

---
+++ exec-c/calculadora/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=calculadora
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/calculadora
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=calculadora.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/calculadora.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=calculadora
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/calculadora
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=calculadora.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/calculadora.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
