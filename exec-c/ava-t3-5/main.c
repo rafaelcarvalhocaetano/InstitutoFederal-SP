33ca1a96314b6148f996c9be9ce2a5874fa8bbd1
Rafael <rapha.pse@outlook.com>
2017-09-08 12:06:55 -0300

    Prova IFSP

---
+++ exec-c/ava-t3-5/main.c
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Rafael Carvalho
+ *
+ * Created on 8 de Setembro de 2017, 11:57
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int x = 0;
+    while(x<10){
+        printf("\nO valor de x eh %d ", x);
+        
+    }
+    system("pause");
+    return 0;
+}
+
