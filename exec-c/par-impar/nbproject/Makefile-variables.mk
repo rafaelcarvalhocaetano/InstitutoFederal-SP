9c2415959ccdb0b32ccc62ddfcd1cfe9d1ba4f3b
Rafael <rapha.pse@outlook.com>
2017-09-09 10:35:08 -0300

    Exec verificando se é par ou impar

---
+++ exec-c/par-impar/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=par-impar
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/par-impar
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=par-impar.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/par-impar.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=par-impar
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/par-impar
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=par-impar.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/par-impar.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
