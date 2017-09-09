1dbafa634467a613574e397d841f7b802470a364
Rafael <rapha.pse@outlook.com>
2017-09-09 10:32:50 -0300

    Exec

---
+++ exec-c/escolha/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=escolha
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/escolha
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=escolha.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/escolha.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=escolha
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/escolha
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=escolha.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/escolha.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
