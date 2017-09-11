45435fc57141f4ee09f6f38ff52176bc38184557
Rafael <rapha.pse@outlook.com>
2017-09-11 20:19:13 -0300

    Usando vetores

---
+++ exec-c/vetores/main.c
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Rafael Carvalho
+ *
+ * Created on 11 de Setembro de 2017, 20:04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    int numero[8];
+    int cont;
+    int ocorrencia = 0;
+    int valor;
+    
+    /*
+     
+    // recebe os números
+    for(cont=0; cont<8; cont++){
+        printf("Digite o valor [%d] : \n", cont);
+        scanf("%d", &numero[cont]);
+    }
+    
+    //imprime os números
+    for(cont=0; cont<8; cont++){
+        printf("numeros [%d] : %d \n",cont, numero[cont] );
+    }
+     
+    */
+    /*
+    
+    numero[0] = 1;
+    numero[1] = 2;
+    numero[2] = 3;
+     
+     */
+    
+    
+    printf("OS VALORES SAO %d %d %d", numero[0], numero[1], numero[2]);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
