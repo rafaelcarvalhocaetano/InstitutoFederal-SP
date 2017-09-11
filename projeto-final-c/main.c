dbd2766d7638b60987788370245fa6c233771459
Rafael <rapha.pse@outlook.com>
2017-09-11 20:04:10 -0300

    Criando projeto final IFSP em C

---
+++ projeto-final-c/main.c
@@ -0,0 +1,31 @@
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M2LPBA
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Rafael Carvalho Caetano
+//******************************************************
+
+/* 
+ * File:   main.c
+ * Author: Rafael Carvalho Caetano
+ *
+ * Created on 11 de Setembro de 2017, 19:57
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+   
+   
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
