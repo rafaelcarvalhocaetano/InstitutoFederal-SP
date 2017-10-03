2859613af108a6c9c206a65f99b40c8a3b264df0
Rafael <rapha.pse@outlook.com>
2017-10-03 10:28:31 -0300

    Table UX UI com integração ao MYSQL - IFSP

---
+++ desenvolvimento_JAVA/DB001-IFSP/src/com/tabela/db/bean/Cliente.java
@@ -0,0 +1,86 @@
+
+package com.tabela.db.bean;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Rafael Carvalho Caetano
+ */
+public class Cliente {
+    
+    private int id;
+    private String nome;
+    private String sobreNome;
+    private Date dataNascimento;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sobreNome
+     */
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    /**
+     * @param sobreNome the sobreNome to set
+     */
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
