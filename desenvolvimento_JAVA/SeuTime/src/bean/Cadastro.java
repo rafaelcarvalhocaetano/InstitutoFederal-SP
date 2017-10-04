054fb143ebb4feb1075b07d676737f1a6ac64eab
Rafael <rapha.pse@outlook.com>
2017-10-04 20:46:53 -0300

    Projeto Cadastro MYSQL com Swing

---
+++ desenvolvimento_JAVA/SeuTime/src/bean/Cadastro.java
@@ -0,0 +1,115 @@
+
+package bean;
+
+/**
+ *
+ * @author Rafael Carvalho Caetano
+ */
+public class Cadastro {
+    
+    private String id;
+    private String nome;
+    private String sobreNome;
+    private String time;
+    private String numeroCamisa;
+    private String cpf;
+    private String dataNascimento;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sobreNome
+     */
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    /**
+     * @param sobreNome the sobreNome to set
+     */
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    /**
+     * @return the time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the numeroCamisa
+     */
+    public String getNumeroCamisa() {
+        return numeroCamisa;
+    }
+
+    /**
+     * @param numeroCamisa the numeroCamisa to set
+     */
+    public void setNumeroCamisa(String numeroCamisa) {
+        this.numeroCamisa = numeroCamisa;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
