445555ad576919899c968465a48368c09235854c
Rafael <rapha.pse@outlook.com>
2017-10-04 20:42:21 -0300

    Projeto Cadastro MYSQL com Swing

---
+++ desenvolvimento_JAVA/SeuTime/src/factory/ConexaoFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Rafael Carvalho Caetano
+ */
+public class ConexaoFactory {
+    
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/time";
+    private static final String USER = "root";
+    private static final String PASS = "q1w2e3r4";
+    
+    
+    public static Connection getConection() throws SQLException{
+        
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        return con;
+    }
+}
