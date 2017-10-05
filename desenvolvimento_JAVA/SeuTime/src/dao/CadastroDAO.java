7d7c7e31ae1b8ee1a44be23f294dbaf804787e83
Rafael <rapha.pse@outlook.com>
2017-10-04 21:48:48 -0300

    Projeto Cadastro MYSQL com Swing

--- desenvolvimento_JAVA/SeuTime/src/dao/CadastroDAO.java
+++ desenvolvimento_JAVA/SeuTime/src/dao/CadastroDAO.java
@@ -6,10 +6,6 @@ import factory.ConexaoFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.sql.Date;
-import javax.swing.JOptionPane;
-import javax.swing.plaf.RootPaneUI;
 
 /**
  *
@@ -20,12 +16,12 @@ public class CadastroDAO {
     
     public void salvar(Cadastro c ){
         
-        
-        String sql = "INSERT INTO (nome, sobreNome, time, numeroCamisa, cpf, dataNascimento) VALUES (?, ?, ?, ?, ?, ?)";
+        StringBuilder sql = new StringBuilder();
+        sql.append("INSERT INTO jogadores (nome, sobreNome, time, numeroCamisa, cpf, dataNascimento) VALUES (?, ?, ?, ?, ?, ?) ");
         
         try {
             Connection con = ConexaoFactory.getConection();
-            PreparedStatement ps = con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql.toString());
             
             ps.setString(1, c.getNome());
             ps.setString(2, c.getSobreNome());
@@ -33,12 +29,12 @@ public class CadastroDAO {
             ps.setString(4, c.getNumeroCamisa());
             ps.setString(5, c.getCpf());
             ps.setString(6, c.getDataNascimento());
-            
             ps.executeUpdate();
             System.out.println("SALVO COM SUCESSO ...");
             
         } catch (SQLException ex) {
             System.out.println("Erro NO salvar");
+           // ex.printStackTrace();
         }
         
     }
