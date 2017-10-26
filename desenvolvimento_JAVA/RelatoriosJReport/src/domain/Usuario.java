9c9ed225deff5fcc2f251f93e099f28bbd62e839
Rafael <rapha.pse@outlook.com>
2017-10-26 00:02:49 -0200

    Teste de Relatórios em PDF

---
+++ desenvolvimento_JAVA/RelatoriosJReport/src/domain/Usuario.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author Developer
+ */
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String placa;
+    private String local;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the local
+     */
+    public String getLocal() {
+        return local;
+    }
+
+    /**
+     * @param local the local to set
+     */
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    
+    
+    
+    
+}
