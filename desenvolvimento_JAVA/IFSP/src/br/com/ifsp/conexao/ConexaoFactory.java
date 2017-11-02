f00d4693904a7c6b8bbef64effdcca2c077c484e
Rafael <rapha.pse@outlook.com>
2017-11-02 11:48:03 -0200

    Projeto Final de java IFSP

--- desenvolvimento_JAVA/IFSP/src/br/com/ifsp/conexao/ConexaoFactory.java
+++ desenvolvimento_JAVA/IFSP/src/br/com/ifsp/conexao/ConexaoFactory.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Rafael Carvalho Caetano
+//******************************************************
 package br.com.ifsp.conexao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,11 +17,16 @@ import javax.swing.JOptionPane;
  */
 public class ConexaoFactory {
     
+    //variavél responsável por conter o usuário do mysql
     private static final String USUARIO = "root";
+    
+    //variavél responsável por conter a senha do mysql
     private static final String SENHA = "q1w2e3r4";
+    
+    //variavél responsável por conter o endereço do banco de dados mysql
     private static final String URL = "jdbc:mysql://localhost:3306/cursos";
     
-    
+    //Responsável por abrir uma conexão com banco mysql
     public static Connection getDB(){
         
         try {
@@ -34,6 +38,7 @@ public class ConexaoFactory {
             throw new RuntimeException("Erro na Conexao");
         }
     }
+    //Responsável por fechar a conexão com banco mysql
    public static void fechaConexao(Connection c){
        if(c != null){
            try {
--- desenvolvimento_JAVA/IFSP/src/br/com/ifsp/domain/Curso.java
+++ desenvolvimento_JAVA/IFSP/src/br/com/ifsp/domain/Curso.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+///******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Rafael Carvalho Caetano
+//******************************************************
 package br.com.ifsp.domain;
 
 /**
@@ -10,5 +11,83 @@ package br.com.ifsp.domain;
  * @author Developer
  */
 public class Curso {
+    
+    private int id;
+    private int cargaHoraria;
+    private String nomeDisciplina;
+    private String CursoPertence;
+    private String periodo;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the cargaHoraria
+     */
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    /**
+     * @param cargaHoraria the cargaHoraria to set
+     */
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    /**
+     * @return the nomeDisciplina
+     */
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+
+    /**
+     * @param nomeDisciplina the nomeDisciplina to set
+     */
+    public void setNomeDisciplina(String nomeDisciplina) {
+        this.nomeDisciplina = nomeDisciplina;
+    }
+
+    /**
+     * @return the CursoPertence
+     */
+    public String getCursoPertence() {
+        return CursoPertence;
+    }
+
+    /**
+     * @param CursoPertence the CursoPertence to set
+     */
+    public void setCursoPertence(String CursoPertence) {
+        this.CursoPertence = CursoPertence;
+    }
+
+    /**
+     * @return the periodo
+     */
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    /**
+     * @param periodo the periodo to set
+     */
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+    
+    
     
 }
